ca2c8a90117282128b0e5748099a8a9c8183e9d0
Michael Whelehan <mikewatsup@gmail.com>
2012-09-13 11:28:41 +0200

    lol

---
+++ www/js/app.js
@@ -0,0 +1,19 @@
+(function($){
+    function on_pause(){
+        console.log("Device paused");
+    }
+
+    function on_resume(){
+        console.log("Device resumed");
+    }
+
+    function on_device_ready(){
+        alert("Device ready");
+        $(document).bind('pause', on_pause);
+        $(document).bind('resume', on_resume);
+    }
+
+    $(window).load(function() {
+        $(document).bind('deviceready', on_device_ready);
+    });
+})(jQuery);
